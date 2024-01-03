080025a571248cef62b5310081d1853fadf6b5b0
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2024-01-03 20:37:51 -0300

    add ambient light

--- src/app/components/model.jsx
+++ src/app/components/model.jsx
@@ -1,16 +1,15 @@
 'use client'
 import {Suspense, useRef} from 'react'
 import { Canvas, useFrame } from "@react-three/fiber";
-import { useGLTF } from "@react-three/drei";
+import { useGLTF, OrbitControls } from "@react-three/drei";
 
 export default function ModeloPala() {
   return (
     <div className="flex w-full h-[55vh]">
         <Canvas dpr={[1, 2]} camera={{ position: [-4, 15, -20], fov: 5 }}>
-          <directionalLight position={[10, 10, 0]} intensity={2.5} />
-          <directionalLight position={[-10, 10, 5]} intensity={2} />
-          <directionalLight position={[-10, 20, 0]} intensity={2.5} />
-          <directionalLight position={[0, -10, 0]} intensity={0.5} />
+          <OrbitControls/>
+          <directionalLight position={[1, 2, 2]} intensity={2.5} />
+          <ambientLight intensity={5.5} />
           <Rotate position-y={1} scale={2}>
             <Suspense fallback={<Model url="/old_shovel.glb" />}>
               <Model url="/old_shovel.glb" />
